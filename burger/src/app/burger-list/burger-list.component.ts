169a683fb81adf557c3811a6f86e57ecb3ce2489
mael-brdn <mael.bourdon@outlook.fr>
2020-05-01 19:59:23 +0200

    Responsive CSS implementation

--- burger/src/app/burger-list/burger-list.component.ts
+++ burger/src/app/burger-list/burger-list.component.ts
@@ -9,14 +9,33 @@ import { BurgersService } from '../api_burger/api/burgers.service';
 export class BurgerListComponent implements OnInit {
 
   burgerList;
+  breakpoint: number;
+  colspan: number;
 
-  constructor(private burgersService : BurgersService) { }
+  constructor(private burgersService: BurgersService) { }
 
   ngOnInit() {
+    this.breakpoint = (window.innerWidth <= 800) ? 1 : 6;
+    this.setColspan(this.breakpoint);
     this.getBurgers();
   }
 
   getBurgers(): void {
     this.burgerList = this.burgersService.listBurgers().subscribe(burgerList => this.burgerList = burgerList);
   }
+
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 800) ? 1 : 6;
+    this.setColspan(this.breakpoint);
+  }
+
+  setColspan(breakpoint: number){
+    if (this.breakpoint === 1){
+      this.colspan = 1;
+    }
+    else {
+      this.colspan = 2;
+    }
+  }
+
 }
